dc6313d5854378ed1c118ca529d6cb401f75e6ea
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-10-01 07:38:31 +0530

    Update axios.js
    api backend added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://messuer-backend.vercel.app/api" : "/api",
   withCredentials: true,
 });
 
