8017ad6bba4fa50058ff36acecd8f84881a835ea
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-12-07 18:33:36 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,44 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 import authRoutes from "./routes/auth.routes.js";  
 import messageRoutes from "./routes/message.route.js";
-
 import { connectDB } from "./lib/db.js"; 
  
 dotenv.config(); 
 const app = express();
 const PORT = process.env.PORT|| 5001;
-const __dirname = path.resolve();
-console.log("sd")
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
-// app.use(cors({
-//   origin: "*",
-//    methods: ["GET", "POST", "PUT", "DELETE"],
-// }));
+ 
 app.use(cors({
-  origin: "https://messuer.vercel.app",//https://chat-application-six-wheat.vercel.app
+  origin: [
+    "https://messuer.vercel.app",
+    "https://yourchats.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
  
 app.use("/api/auth", authRoutes); 
 app.use("/api/messages", messageRoutes);  
-
- // if(process.env.NODE_ENV==="production"){
-//   app.use(express.static(path.join(__dirname,"../frontend/dist")));
-
-//   app.get("*",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-//   })
-// }
-
-
+ 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
   connectDB();
