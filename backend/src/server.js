bfb4493ee336f24242c48047972020b5782aef36
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-12-07 16:44:54 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 //    methods: ["GET", "POST", "PUT", "DELETE"],
 // }));
 app.use(cors({
-  origin: "*",//https://chat-application-six-wheat.vercel.app
+  origin: "http://localhost:5173",//https://chat-application-six-wheat.vercel.app
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
