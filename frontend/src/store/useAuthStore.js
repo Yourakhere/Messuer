f56724b080cf6c0cad315acc2fa38dc20f84bef1
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-12-07 16:17:44 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL ="https://chat-backend-ten-amber.vercel.app";
+const BASE_URL ="https://messuer-backend.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
