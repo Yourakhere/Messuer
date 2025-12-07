2a91257a326de4caaddde5b2eec5f7a28a7da1f3
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-12-07 16:18:17 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
  
-  baseURL: "https://chat-backend-ten-amber.vercel.app/api",
+  baseURL: "https://messuer-backend.onrender.com/api",
   withCredentials: true,
 });
 
